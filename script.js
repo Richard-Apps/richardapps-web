e58eeeefde474320388e4bbf8ad871b70b1b11fd
RichardApps <66631299+Richard-Apps@users.noreply.github.com>
2024-03-25 15:32:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+fetch('src/ascii.txt')
+    .then(res => res.text())
+    .then(res => document.getElementById('title').innerHTML = res)
+
+document.getElementById('noise_vid').playbackRate = 0.65;
+
+function changeTab(tab) {
+    document.querySelectorAll('.tabs').forEach(element => {
+        element.classList.add('invisible')
+    });
+    document.getElementById(tab).classList.remove('invisible')
+}
